c191b87c14d4a365795f727fae3fe41f1e9736f5
asterfei <hefei@seas.upenn.edu>
2013-10-11 19:28:02 -0300

    Revert modofocation

--- src/com/example/medicinereminder/MainActivity.java
+++ src/com/example/medicinereminder/MainActivity.java
@@ -17,7 +17,6 @@ import com.parse.ParseAnalytics;
 public class MainActivity extends Activity {
 	public static final int LoginActivity_ID = 1;
 	
-	//this is a test modification
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
