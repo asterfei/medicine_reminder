2708ec651ea931d037aac8151df7cad56f5ecf4a
yingwa <yingwa@seas.upenn.edu>
2013-11-26 22:33:48 -0500

    commit by yingwa
    add Calendar view

---
+++ src/com/example/medicinereminder/CaldroidSampleActivity.java
@@ -0,0 +1,190 @@
+package com.example.medicinereminder;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+@SuppressLint("SimpleDateFormat")
+public class CaldroidSampleActivity extends FragmentActivity {
+	private boolean undo = false;
+	private CaldroidFragment caldroidFragment;
+	private CaldroidFragment dialogCaldroidFragment;
+
+	/*private void setCustomResourceForDates() {
+		Calendar cal = Calendar.getInstance();
+
+		//Min date is last 7 days
+		cal.add(Calendar.DATE, -18);
+		Date blueDate = cal.getTime();
+
+		 //Max date is next 7 days
+		cal = Calendar.getInstance();
+		cal.add(Calendar.DATE, 16);
+		Date greenDate = cal.getTime();
+
+		if (caldroidFragment != null) {
+			caldroidFragment.setBackgroundResourceForDate(R.color.blue,
+					blueDate);
+			caldroidFragment.setBackgroundResourceForDate(R.color.green,
+					greenDate);
+			caldroidFragment.setTextColorForDate(R.color.white, blueDate);
+			caldroidFragment.setTextColorForDate(R.color.white, greenDate);
+		}
+	}*/
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_calendar);
+
+		final SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
+
+		// Setup caldroid fragment
+		// **** If you want normal CaldroidFragment, use below line ****
+		caldroidFragment = new CaldroidFragment();
+
+		// //////////////////////////////////////////////////////////////////////
+		// **** This is to show customized fragment. If you want customized
+		// version, uncomment below line ****
+		// caldroidFragment = new CaldroidSampleCustomFragment();
+
+		// Setup arguments
+
+		// If Activity is created after rotation
+		if (savedInstanceState != null) {
+			caldroidFragment.restoreStatesFromKey(savedInstanceState,
+					"CALDROID_SAVED_STATE");
+		}
+		// If activity is created from fresh
+		else {
+			Bundle args = new Bundle();
+			Calendar cal = Calendar.getInstance();
+			args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
+			args.putInt(CaldroidFragment.YEAR, cal.get(Calendar.YEAR));
+			args.putBoolean(CaldroidFragment.ENABLE_SWIPE, true);
+			args.putBoolean(CaldroidFragment.SIX_WEEKS_IN_CALENDAR, true);
+
+			// Uncomment this to customize startDayOfWeek
+			// args.putInt(CaldroidFragment.START_DAY_OF_WEEK,
+			// CaldroidFragment.TUESDAY); // Tuesday
+			caldroidFragment.setArguments(args);
+		}
+
+		//setCustomResourceForDates();
+
+		// Attach to the activity
+		FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+		t.replace(R.id.calendar1, caldroidFragment);
+		t.commit();
+
+		// Setup listener
+		final CaldroidListener listener = new CaldroidListener() {
+
+			@Override
+			public void onSelectDate(Date date, View view) {
+				Toast.makeText(getApplicationContext(), formatter.format(date),
+						Toast.LENGTH_SHORT).show();
+
+			}
+
+			@Override
+			public void onChangeMonth(int month, int year) {
+				String text = "month: " + month + " year: " + year;
+				Toast.makeText(getApplicationContext(), text,
+						Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onLongClickDate(Date date, View view) {
+				Toast.makeText(getApplicationContext(),
+						"Long click " + formatter.format(date),
+						Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onCaldroidViewCreated() {
+				if (caldroidFragment.getLeftArrowButton() != null) {
+					Toast.makeText(getApplicationContext(),
+							"Caldroid view is created", Toast.LENGTH_SHORT)
+							.show();
+				}
+			}
+
+		};
+
+		// Setup Caldroid
+		caldroidFragment.setCaldroidListener(listener);
+
+		final TextView textView = (TextView) findViewById(R.id.textview);
+		Button showDialogButton = (Button) findViewById(R.id.show_dialog_button);
+
+		final Bundle state = savedInstanceState;
+		showDialogButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// Setup caldroid to use as dialog
+				dialogCaldroidFragment = new CaldroidFragment();
+				dialogCaldroidFragment.setCaldroidListener(listener);
+
+				// If activity is recovered from rotation
+				final String dialogTag = "CALDROID_DIALOG_FRAGMENT";
+				if (state != null) {
+					dialogCaldroidFragment.restoreDialogStatesFromKey(
+							getSupportFragmentManager(), state,
+							"DIALOG_CALDROID_SAVED_STATE", dialogTag);
+					Bundle args = dialogCaldroidFragment.getArguments();
+					if (args == null) {
+						args = new Bundle();
+						dialogCaldroidFragment.setArguments(args);
+					}
+					args.putString(CaldroidFragment.DIALOG_TITLE,
+							"Select a date");
+				} else {
+					// Setup arguments
+					Bundle bundle = new Bundle();
+					// Setup dialogTitle
+					bundle.putString(CaldroidFragment.DIALOG_TITLE,
+							"Select a date");
+					dialogCaldroidFragment.setArguments(bundle);
+				}
+
+				dialogCaldroidFragment.show(getSupportFragmentManager(),
+						dialogTag);
+			}
+		});
+	}
+
+	/**
+	 * Save current states of the Caldroid here
+	 */
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+
+		if (caldroidFragment != null) {
+			caldroidFragment.saveStatesToKey(outState, "CALDROID_SAVED_STATE");
+		}
+
+		if (dialogCaldroidFragment != null) {
+			dialogCaldroidFragment.saveStatesToKey(outState,
+					"DIALOG_CALDROID_SAVED_STATE");
+		}
+	}
+
+}
